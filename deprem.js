973deec77ba2406e06ab66c60a6d2588210a590d
devran yıldız <devranarda58@gmail.com>
2024-08-11 21:35:21 +0300

    Deprem Projesi
    Türkiyede olan depremler anlık olarak sitede gözüküyor

---
+++ deprem.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const tableBody = document.getElementById('dataTable').querySelector('tbody');
+
+    try {
+        const response = await fetch('https://localhost:7113/Earthquake');
+        if (!response.ok) {
+            throw new Error('Failed to fetch');
+        }
+        const data = await response.json();
+        data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+        // Tabloyu doldur
+        tableBody.innerHTML = data.map(event => `
+            <tr>
+                <td>${new Date(event.date).toLocaleString()}</td>
+                <td>${event.latitude}</td>
+                <td>${event.longitude}</td>
+                <td>${event.depth}</td>
+                <td>${event.magnitude}</td>
+                <td>${event.type}</td> <!-- type ekledik -->
+                <td>${event.location}</td>
+            </tr>
+        `).join('');
+    } catch (error) {
+        console.error('Error fetching earthquake data:', error);
+        document.getElementById('error').innerText = `Unable to load earthquake data. Error: ${error.message}`;
+    }
+});
